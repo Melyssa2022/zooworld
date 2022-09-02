4d90757f7dff65db703ae18986354977b3ffe521
Mel <108138017+Melyssa2022@users.noreply.github.com>
2022-09-02 12:25:42 -0300

    Add files via upload

---
+++ cad.js
@@ -0,0 +1,31 @@
+let nome = document.querySelector('#nome')
+let alimentacao = document.querySelector('#alimentacao')
+
+function cadastrar(){
+    let listaAnimais = localStorage.getItem('listaAnimais')
+
+    let linha = `<tr>`
+        linha += `<td>${nome.value}</td>`
+        linha += `<td>${nomecientifico.value}</td>`
+        linha += `<td>${especie.value}</td>`
+        linha += `<td>${qtd.value}</td>`
+        linha += `<td>${peso.value}</td>`
+        linha += `<td>${alimentacao.value}</td>`
+
+    linha += `</tr>`
+    
+    if(listaAnimais == null){
+       listaAnimais = linha
+    }else{
+        listaAnimais += linha
+    }
+       
+    localStorage.setItem('listaAnimais',listaAnimais)
+    msgSucesso.innerHTML = '<strong>Cadastrado com sucesso....</strong>'
+setTimeout(() => {
+    document.location.reload(true)
+
+}, 3000);
+
+
+}
